95a56f7b4bcc1db5877f52762e9d06aeb1fceff8
TamannaBagwan <144624297+TamannaBagwan@users.noreply.github.com>
2023-10-16 18:35:33 +0530

    committing before switching

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Order />} />
           <Route path="/orderDetails" element={<OrderDetails />} />
-
         </Routes>
       </BrowserRouter>
     </>
